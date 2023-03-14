28271a8b86dd5307b3239d0d05f3db1e389597e2
Iishaan <iishaanjha30@gmail.com>
2023-03-15 00:28:08 +0530

    Updating Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import Navbar from './components/navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <TodoList />
       
     </div>
---
+++ src/components/navbar.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import "./navstyle.css";
+import {GiHamburgerMenu} from "react-icons/gi";
+
+const Navbar = () => {
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
+  return (
+    <>
+        <nav className='main-nav'>
+        <div className='logo'>
+            <h2>TaskMate</h2>
+        </div>
+
+        <div className = {showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"}> 
+            <ul>
+                <li>
+                    <a href="#">Todo</a>
+                </li>
+            
+                <li>
+                    <a href="#">Project</a>
+                </li>
+            
+                <li>
+                    <a href="#">Team</a>
+                </li>
+            </ul>
+            <div className='hamburger-menu'>
+                <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                    <GiHamburgerMenu />
+                </a>
+            </div>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
